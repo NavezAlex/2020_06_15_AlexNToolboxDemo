da66ac7095ce7e189b20210ee768d693fbf119a8
NavezAlex <navez.alex@gmail.com>
2020-06-15 14:25:24 +0200

    Change Color

---
+++ ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class ChangeColor : MonoBehaviour
+{
+    [SerializeField]
+    Color _colorToChange, _colorToReturn;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.GetComponent<Renderer>().material.color = _colorToChange;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            gameObject.GetComponent<Renderer>().material.color = _colorToReturn;
+        }
+    }
+}
