e11f7b7cda7060a390c8db38c9f7623f25c3d35f
NavezAlex <navez.alex@gmail.com>
2020-06-18 16:14:28 +0200

    Teleportation Script

---
+++ TeleportInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TeleportInput : MonoBehaviour
+{
+    public Teleportation m_targetToInteractWith;
+    public KeyCode m_keyToUse = KeyCode.T;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(m_keyToUse))
+        {
+            m_targetToInteractWith.StartTeleport();
+        }
+        if (Input.GetKeyUp(m_keyToUse))
+        {
+            m_targetToInteractWith.StopTeleport();
+        }
+
+    }
+
+}
---
+++ Teleportation.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+//For the creation of the Laser
+[RequireComponent(typeof(LineRenderer))]
+public class Teleportation : MonoBehaviour
+{
+    [SerializeField]
+    Transform _head, _cameraRig;
+
+    [SerializeField]
+    LineRenderer _laser;
+    [SerializeField]
+    int _laserSteps = 20;
+    [SerializeField]
+    float _laserSegmentDistance = 1f, _dropPerSegment = .1f;
+
+    //Layer where we can teleport
+    [SerializeField]
+    int _targetLayer;
+
+    private Vector3 _targetPos;
+    private Vector3 _currentPos;
+
+    bool _tpTarget = false;
+
+    private void Update()
+    {
+        if (_tpTarget)
+        {
+            RecordCurrentPos();
+            StartDisplayRay();
+        }
+        else if (!_tpTarget)
+        {
+            ApplyTeleport();
+            StopDisplayRay();
+        }
+    }
+
+    //See TeleportInput for using
+    public void StartTeleport()
+    {
+        _tpTarget = true;
+    }
+    public void StopTeleport()
+    {
+        _tpTarget = false;
+    }
+
+
+    private void RecordCurrentPos()
+    {
+        _currentPos = transform.position;
+    }
+
+    //Creation of the Ray
+    private void StartDisplayRay()
+    {
+        _tpTarget = false;
+        RaycastHit _hit;
+        Vector3 _origine = _currentPos;
+        _laser.SetPosition(0, _origine);
+
+        for (int i = 0; i < _laserSteps - 1; i++)
+        {
+            Vector3 _offset = (transform.forward + (Vector3.down * _dropPerSegment * i)).normalized * _laserSegmentDistance;
+
+            if (Physics.Raycast(_origine, _offset, out _hit, _laserSegmentDistance))
+            {
+                for (int j = i + 1; j < _laser.positionCount; j++)
+                {
+                    _laser.SetPosition(j, _hit.point);
+                }
+
+                //Check if the zone targeted is allowed
+                if (_hit.transform.gameObject.layer == _targetLayer)
+                {
+                    //Before, Apply a material color in LineRenderer
+                    _laser.startColor = _laser.endColor = Color.green;
+                    _targetPos = _hit.point;
+                    _tpTarget = true;
+                    return;
+                }
+                else
+                {
+                    _laser.startColor = _laser.endColor = Color.red;
+                    return;
+                }
+            }
+            else
+            {
+                _laser.SetPosition(1 + i, _origine + _offset);
+                _origine += _offset;
+            }
+        }
+        _laser.startColor = _laser.endColor = Color.red;
+    }
+
+    private void ApplyTeleport()
+    {
+        _tpTarget = false;
+
+        Vector3 _offset = new Vector3(_targetPos.x - _head.transform.position.x, _targetPos.y - _cameraRig.position.y, _targetPos.z - _head.transform.position.z);
+
+        _cameraRig.position += _offset;
+    }
+
+    //Reset the Ray
+    private void StopDisplayRay()
+    {
+        for (int i = 0; i < _laser.positionCount; i++)
+        {
+            _laser.SetPosition(i, Vector3.zero);
+        }
+    }
+}
